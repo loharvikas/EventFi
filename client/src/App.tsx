e9187ac5102bdf4325aee06098145a5a9c7e55a3
loharvikas <loharvikas13@gmail.com>
2024-09-24 22:28:42 +0530

    chore: initial setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import theme from './designSystem';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Signin from './pages/Signin/Signin';
+import Singup from './pages/Signup/Signup';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Signin />,
+  },
+  {
+    path: "/signup",
+    element: <Singup />,
+    
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme/>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
