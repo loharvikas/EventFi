78343c5c7b73adfb220ae659fc3a6084e12c1b52
loharvikas <loharvikas13@gmail.com>
2024-09-27 20:08:27 +0530

    feat: event feature

--- client/src/pages/Events/CreateEvent.tsx
+++ client/src/pages/Events/CreateEvent.tsx
@@ -6,6 +6,8 @@ import {
   Typography,
   Modal,
   Stack,
+  Grid,
+  Grid2
 } from '@mui/material';
 import { Link } from 'react-router-dom';
 import EventFiCard from '../../components/EventFiCard/EventFiCard';
@@ -14,103 +16,126 @@ import EventFiButton from '../../components/EventFiButton/EventFiButton';
 
 
 const EventCreationForm = () => {
-  const [guestList, setGuestList] = useState<string[]>([]);
-  const [openGuestModal, setOpenGuestModal] = useState(false);
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     // Event creation logic goes here
   };
 
-  const handleGuestSave = (newGuestList: string[]) => {
-    setGuestList(newGuestList);
-    setOpenGuestModal(false);
-  };
-
-  const handleGuestModalOpen = () => {
-    setOpenGuestModal(true);
-  };
-
-  const handleGuestModalClose = () => {
-    setOpenGuestModal(false);
-  };
-
   return (
-    <Stack>
-      <EventFiCard variant="outlined">
-        <Typography variant="h3">Create Event</Typography>
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100%',
-            gap: 2,
-          }}
-        >
-          <FormControl>
-            <FormLabel htmlFor="eventName">Event Name</FormLabel>
+    <EventFiCard variant="outlined">
+    <Typography variant="h5">Create event</Typography>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      noValidate
+      sx={{
+        width: '100%',
+        mt: 2,
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="eventName">Event Name *</FormLabel>
             <EventFiTextField
               id="eventName"
-              type="text"
               name="eventName"
               placeholder="Event Name"
               required
               fullWidth
-              variant="outlined"
             />
           </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="eventDate">Event Date</FormLabel>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="eventDate">Event Date *</FormLabel>
             <EventFiTextField
               id="eventDate"
               type="date"
               name="eventDate"
               required
               fullWidth
-              variant="outlined"
             />
           </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="venue">Venue</FormLabel>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="addressLine1">Address Line 1 *</FormLabel>
             <EventFiTextField
-              id="venue"
-              type="text"
-              name="venue"
-              placeholder="Event Venue"
+              id="addressLine1"
+              name="addressLine1"
+              placeholder="Address Line 1"
               required
               fullWidth
-              variant="outlined"
             />
           </FormControl>
-
-          <FormControl>
-            <FormLabel htmlFor="guestList">Guest List</FormLabel>
-            <EventFiButton
-              type="button"
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="addressLine2">Address Line 2</FormLabel>
+            <EventFiTextField
+              id="addressLine2"
+              name="addressLine2"
+              placeholder="Address Line 2"
               fullWidth
-              variant="outlined"
-              onClick={handleGuestModalOpen}
-              label="Add Guest List"
             />
-            {guestList.length > 0 && (
-              <Typography>{guestList.length} Guests Added</Typography>
-            )}
           </FormControl>
-
-          <EventFiButton type="submit" fullWidth variant="contained" label="Create event" />
-        </Box>
-      </EventFiCard>
-
-      {/* Guest List Modal */}
-      {/* <Modal open={openGuestModal} onClose={handleGuestModalClose}>
-        <GuestListForm onSave={handleGuestSave} onClose={handleGuestModalClose} />
-      </Modal> */}
-    </Stack>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="city">City *</FormLabel>
+            <EventFiTextField
+              id="city"
+              name="city"
+              placeholder="City"
+              required
+              fullWidth
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="state">State *</FormLabel>
+            <EventFiTextField
+              id="state"
+              name="state"
+              placeholder="State"
+              required
+              fullWidth
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="zipCode">Zip Code *</FormLabel>
+            <EventFiTextField
+              id="zipCode"
+              name="zipCode"
+              placeholder="Zip Code"
+              required
+              fullWidth
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel htmlFor="country">Country *</FormLabel>
+            <EventFiTextField
+              id="country"
+              name="country"
+              placeholder="Country"
+              required
+              fullWidth
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <EventFiButton type="submit" fullWidth variant="contained" label='Create event' />
+        </Grid>
+      </Grid>
+    </Box>
+  </EventFiCard>
   );
 };
 
--- client/src/pages/Events/Events.tsx
+++ client/src/pages/Events/Events.tsx
@@ -2,14 +2,16 @@ import { Box, Modal, Stack, Typography } from "@mui/material";
 import EventFiButton from "../../components/EventFiButton/EventFiButton";
 import EventCreationForm from "./CreateEvent";
 import { useState } from "react";
+import EventFiModal from "../../components/EventFiModal/EventFiModal";
+import { GridColDef } from "@mui/x-data-grid";
+import EventData from "./EventData";
 
 
 const Events = () => {
     const [openEventForm, setOpenEventForm] = useState<boolean>(false);
-
     return (
         <Box >
-            <Stack direction="row" justifyContent="space-between" alignItems="center" borderBottom={1} borderColor="divider" padding={1}>
+            <Stack direction="row" justifyContent="space-between" alignItems="center" borderBottom={1} borderColor="divider" paddingTop={0.5} paddingBottom={0.5} paddingLeft={1} paddingRight={1}>
                 <Stack>
                     <Typography variant="subtitle1">Events</Typography>
                 </Stack>
@@ -17,9 +19,12 @@ const Events = () => {
                     <EventFiButton label="Create Event" variant="text"  onClick={() => setOpenEventForm(true)} />
                 </Stack>
             </Stack>
-            <Modal open={openEventForm} onClose={() => setOpenEventForm(false)}>
+            <EventFiModal open={openEventForm} onClose={() => setOpenEventForm(false)}>
                 <EventCreationForm />
-            </Modal>
+            </EventFiModal>
+            <Stack>
+                <EventData />
+            </Stack>
         </Box>
     )
 }
